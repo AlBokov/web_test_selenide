77d2599f84c9e5768d0389c2b296cfcba5bcd096
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-05-31 11:17:13 +0300

    correct build

---
+++ src/test/java/ru/netology/web/formTestV1.java
@@ -0,0 +1,26 @@
+//package ru.netology.web;
+//import com.codeborne.selenide.Condition;
+//import com.codeborne.selenide.Selenide;
+//import com.codeborne.selenide.SelenideElement;
+//import org.junit.jupiter.api.Test;
+//
+//import static com.codeborne.selenide.Condition.exactText;
+//import static com.codeborne.selenide.Selenide.$;
+//
+//
+//public class formTestV1 {
+//
+//
+//    // проверка формы с валлидными данными
+//    @Test
+//    void workingWithValidData() {
+//        Selenide.open("http://0.0.0.0:9999");
+//        SelenideElement form = $(".form_theme_alfa-on-white");
+//        form.$("[name=name]").setValue("Боков Александр");
+//        form.$("[name=phone]").setValue("+95000000000");
+//        form.$(".checkbox_theme_alfa-on-white").click();
+//        form.$("button").click();
+//        $(".paragraph").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+//
+//    }
+//}
