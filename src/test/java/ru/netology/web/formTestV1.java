acf00a437ec2a4ffcdcb87565b8a5cec92978c7e
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-06-03 21:28:11 +0300

    Исправление selenide

--- src/test/java/ru/netology/web/formTestV1.java
+++ src/test/java/ru/netology/web/formTestV1.java
@@ -1,26 +1,56 @@
 package ru.netology.web;
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class formTestV1 {
+    private WebDriver driver;
 
+    // Настройка и установка webdriver
+    @BeforeAll
+    public static void setupAll(){
+        WebDriverManager.chromedriver().setup();
+    }
 
-    // проверка формы с валлидными данными
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage"); // Аргумент для работы с памятью в CI
+        options.addArguments("--no-sandbox"); // Аргумент для работы с памятью в CI
+        options.addArguments("--headless"); // Этот аргумент отвечает за режим headless
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+
     @Test
-    void workingWithValidData() {
-        Selenide.open("http://0.0.0.0:9999");
-        SelenideElement form = $(".form_theme_alfa-on-white");
-        form.$("[name=name]").setValue("Боков Александр");
-        form.$("[name=phone]").setValue("+95000000000");
-        form.$(".checkbox_theme_alfa-on-white").click();
-        form.$("button").click();
-        $(".paragraph").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    public void shouldBeSuccessfulForm() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Боков Александр Александрович");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+95000000000");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        var actualText = actualElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",actualText);
+        assertTrue(actualElement.isDisplayed());
+
 
     }
+
 }
